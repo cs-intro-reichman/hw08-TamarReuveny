92ef44e8529e59d03c56b00c9c6c2d5bb0148ca3
Tamar Reuveny <Tamar.reuveny@runi.ac.il>
2024-02-07 18:55:22 +0200

    edit

--- CombinedOperationsTester.java
+++ CombinedOperationsTester.java
@@ -18,51 +18,43 @@ public class CombinedOperationsTester {
         // Check total duration
         System.out.println("Total Duration: " + playList.totalDuration() + " seconds");
 
-        //Remove the last track and check size
-       playList.removeLast();
-       System.out.println("Last track removed");
-       System.out.println("Playlist Size: " + playList.getSize());
+        // Remove the last track and check size
+        playList.removeLast();
+        System.out.println("Last track removed");
+        System.out.println("Playlist Size: " + playList.getSize());
 
         // Add another track
         playList.add(new Track("Creep", "Radiohead", 369));
         System.out.println("Track added: Creep");
-        System.out.println("Playlist Size: " + playList.getSize());
-
-        // Check total duration
-        System.out.println("Total Duration: " + playList.totalDuration() + " seconds");
-
 
         // Final state of the playlist
-        //System.out.println("Final Playlist Size: " + playList.getSize());
-        //System.out.println("Final Total Duration: " + playList.totalDuration() + " seconds");
+        System.out.println("Final Playlist Size: " + playList.getSize());
+        System.out.println("Final Total Duration: " + playList.totalDuration() + " seconds");
     
 
 
         // Adding tracks at specific indexes
-       playList.add(0, new Track("Yesterday", "The Beatles", 125)); // Add at the beginning
-       //System.out.println(playList);
-       System.out.println("Track 'Yesterday' added at index 0.");
+        playList.add(0, new Track("Yesterday", "The Beatles", 125)); // Add at the beginning
+        System.out.println("Track 'Yesterday' added at index 0.");
         
         playList.add(playList.getSize(), new Track("Bohemian Rhapsody", "Queen", 354)); // Assuming to add at the end
         System.out.println("Track 'Bohemian Rhapsody' added at the end.");
-        //System.out.println(playList);
-
+        
         // Removing a track by index
         playList.remove(1); // This now refers to removing the second track which was "Imagine" initially
         System.out.println("Track at index 1 removed.");
-        //System.out.println(playList);
-
+        
         // Sorting the playlist
         playList.sortedInPlace();
         System.out.println("Playlist sorted by increasing duration.");
-        //System.out.println(playList);
-        //Since we can't directly print sorted tracks, let's focus on aspects we can test
+    
+        // Since we can't directly print sorted tracks, let's focus on aspects we can test
         // Displaying the title of the shortest track after sorting
         System.out.println("Shortest track after sorting: " + playList.titleOfShortestTrack());
         
-      //Final state of the playlist after additional operations
-      System.out.println("Final Playlist Size after additional operations: " + playList.getSize());
-      System.out.println("Final Total Duration after additional operations: " + playList.totalDuration() + " seconds");
+        // Final state of the playlist after additional operations
+        System.out.println("Final Playlist Size after additional operations: " + playList.getSize());
+        System.out.println("Final Total Duration after additional operations: " + playList.totalDuration() + " seconds");
     }
     
 
